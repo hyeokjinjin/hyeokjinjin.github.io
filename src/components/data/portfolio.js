d0f96d2060f2467de7203347bae5c3a44ced094b
Hyeokjin Jin <hyeokjinjin@gmail.com>
2024-05-10 23:40:28 -0500

    Added Project Images

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -1,14 +1,14 @@
 export default [
     {
         title: 'Better Spotify Wrapped',
-        imgUrl: '',
+        imgUrl: '/assets/betterwrap.jpg',
         stack: ['Java', 'SQLite'],
         link: 'https://youtu.be/9bqzak2Abwk',
     },
     {
-        title: 'Better Spotify Wrapped',
-        imgUrl: '',
-        stack: ['Java', 'SQLite'],
-        link: 'https://youtu.be/9bqzak2Abwk',
+        title: 'Personal Developer Portfolio',
+        imgUrl: '/assets/personalport.jpg',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'https://hyeokjinjin.github.io',
     },
 ]
