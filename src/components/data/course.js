8532673eb3e6434d2eb427ed4645d95844379b3c
Hyeokjin Jin <hyeokjinjin@gmail.com>
2024-09-05 20:41:31 -0400

    Updated Courses

--- src/components/data/course.js
+++ src/components/data/course.js
@@ -1,23 +1,31 @@
 export default[
     {
-        name: 'CS 1331',
-        desc: 'Intro to Object Oriented Programming (Java)',
+        name: 'Vertically Integrated Project',
+        desc: 'Automotive LiDAR VIP',
+    },
+    {
+        name: 'CS 2110',
+        desc: 'Computer Organization and Programming',
+    },
+    {
+        name: 'CS 4400',
+        desc: 'Intro to Database Systems',
     },
     {
         name: 'CS 1332',
         desc: 'Data Structures and Algorithms (Java)',
     },
     {
-        name: 'CS 2050',
-        desc: 'Intro to Discrete Mathematics',
+        name: 'CS 1331',
+        desc: 'Intro to Object Oriented Programming (Java)',
     },
     {
         name: 'CS 2340',
         desc: 'Objects and Design (Android Studio, Java)',
     },
     {
-        name: 'VIP 2601',
-        desc: 'Automotive LiDAR VIP',
+        name: 'CS 2050',
+        desc: 'Intro to Discrete Mathematics',
     },
     {
         name: 'MATH 1554',
@@ -26,5 +34,9 @@ export default[
     {
         name: 'MATH 2550',
         desc: 'Intro to Multivariable Calculus',
+    },
+    {
+        name: 'ISYE 3770',
+        desc: 'Statistics and Applications',
     },
 ]
