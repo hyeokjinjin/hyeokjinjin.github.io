14311e1121008490d587955a11bc3b5805f6fbe9
Hyeokjin Jin <hyeokjinjin@gmail.com>
2025-03-16 15:55:02 -0400

    Updated Portfolio

--- src/components/data/portfolio.js
+++ src/components/data/portfolio.js
@@ -2,19 +2,19 @@ export default [
     {
         title: 'Word Helper',
         imgUrl: '/assets/wordfinder.jpg',
-        stack: ['Java', 'Python'],
+        stack: ['Java', 'Python', 'Tesseract OCR'],
         link: 'https://github.com/hyeokjinjin/wordfinder',
     },
     {
         title: 'Better Spotify Wrapped',
         imgUrl: '/assets/betterwrap.jpg',
-        stack: ['Java', 'SQL'],
+        stack: ['Java', 'SQL', 'Spotify API'],
         link: 'https://youtu.be/9bqzak2Abwk',
     },
     {
         title: 'gopü',
         imgUrl: '/assets/gopu.jpg',
-        stack: ['Java', 'Maps SDK', 'SQL'],
+        stack: ['Java', 'SQL', 'Maps SDK'],
         link: 'https://devfolio.co/projects/gopu-f6fe',
     },
     {
